bbe27ca96cc762405f17d06bfb6d998274753164
Peyton <peytonpendley18@gmail.com>
2023-01-31 13:00:02 -0600

    update files

--- src/App.js
+++ src/App.js
@@ -1,36 +1,77 @@
-import './index.css';
-import Nav from './components/Nav.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Footer from './components/Footer';
-import Books from './pages/Books';
-import { books } from './data';
-import BookInfo from './pages/BookInfo';
-import Cart from './pages/Cart';
-import { useEffect, useState } from 'react';
+import "./index.css";
+import Nav from "./components/Nav.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Footer from "./components/Footer";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [ cart, setCart ] = useState([]);
+  const [cart, setCart] = useState([]);
 
-  function addToCart(book){
-    setCart([...cart, {...book, quantity: 1}])
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    console.log("removeitem", item);
+    setCart(cart.filter(book => book.id !== item.id))
+  }
+
+  function numberOfItems(){
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter
   }
 
   useEffect(() => {
-    console.log(cart)
-  }, [cart])
+    console.log(cart);
+  }, [cart]);
 
   return (
     <Router>
       <div className="App">
-          <Nav />
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/books' element={<Books books={books} />} />
-            <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />} />
-            <Route path='/cart' element={<Cart books={books} cart={cart} />} />
-          </Routes>
-          <Footer/>
+        <Nav numberOfItems={numberOfItems()}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={
+              <BookInfo books={books} addToCart={addToCart} cart={cart} />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                books={books}
+                cart={cart}
+                changeQuantity={changeQuantity}
+                removeItem={removeItem}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
       </div>
     </Router>
   );
--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -3,8 +3,6 @@ import Book from './ui/Book';
 import { books } from '../data.js';
 
 const Featured = () => {
-    console.log(books)
-    console.log(books.filter(book => book.rating === 5).slice(0, 4));
     return(
         <section id="features">
             <div className='container'>
